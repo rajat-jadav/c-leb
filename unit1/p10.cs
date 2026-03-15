28baf0b3cfd7b9e37c72eba2205c205630707be6
= <=>
2026-03-15 20:20:21 +0530

    update

--- unit1/p10.cs
+++ unit1/p10.cs
@@ -1,47 +1,12 @@
 using System;
 
-class Factorial
+Console.Write("Enter a number : ");
+int num = int.Parse(Console.ReadLine());
+int fect = 1;
+while(num > 0)
 {
-    
-    static long CalculateFactorial(int num)
-    {
-        if (num < 0)
-        {
-            throw new ArgumentException("Factorial is not defined for negative numbers.");
-        }
-        
-        if (num == 0 || num == 1)
-        {
-            return 1;
-        }
-        
-        long factorial = 1;
-        for (int i = 2; i <= num; i++)
-        {
-            factorial *= i;
-        }
-        return factorial;
-    }
-
-    static void Main()
-    {
-        try
-        {
-            Console.Write("Enter a number to find its factorial: ");
-            
-            if (int.TryParse(Console.ReadLine(), out int number))
-            {
-                long result = CalculateFactorial(number);
-                Console.WriteLine($"Factorial of {number} is: {result}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid input! Please enter a valid integer.");
-            }
-        }
-        catch (ArgumentException ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-    }
+    fect *= num;
+    num--;
 }
+
+Console.WriteLine($"Fectorial of given value is {fect}");
